9cd5d97236721f6005139bc10b78113db8b41a93
yijiahuang <hyj@iastate.edu>
2020-03-29 16:33:56 -0500

    add test ids

--- src/java/boa/functions/paper/BoaStopWords.java
+++ src/java/boa/functions/paper/BoaStopWords.java
@@ -19,22 +19,37 @@ public class BoaStopWords {
 			"no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just",
 			"don", "should", "using", "show", "result", "large", "also", "iv", "previously", "shown", "et", "al", ",",
 			"al.", "al.," };
-	
+
+	private static final String[] testPaperIds = new String[] { "a08c87a957ae94e9d23bb572191e2ded2c16414e",
+			"e89563eb94ead6b73b9bc6efde85843cbfb1653b", "31165346222e30211f9895693dee83cd824e2232",
+			"b1052d3e68d607d53c9ba567094cac528de60fd1", "5722e1b167fe879fc2901ba8ea2fa69fec89a6f7",
+			"62cbd36e69d6db0897bd514bd120772bc8841c1d", "d7823cfc64949e723256c9f860fd028055a42730",
+			"f678918637d6949ca22b1d7cd5982309f8ad6dbe", "7f3dc96aa00c32144d5f11e368d36271eeaf64ff",
+			"0ffc503972e23c8b6c079724b9b24ff59028d323", "84c6fb023cfde66935e2dba83991efefd45b0cc4",
+			"b69be33b29de31a987633b3a382a1bee5f18a954" };
+
 	private static HashSet<String> stopWordsSet;
-	
+	private static HashSet<String> testIds;
+
 	static {
 		stopWordsSet = new HashSet<String>(Arrays.asList(stopWords));
+		testIds = new HashSet<String>(Arrays.asList(testPaperIds));
 	}
-	
+
 	@FunctionSpec(name = "stop_words", returnType = "set of string")
 	public static HashSet<String> stopWords() {
 		return stopWordsSet;
 	}
-	
+
 	@FunctionSpec(name = "update_stop_words", returnType = "set of string", formalParameters = { "string..." })
 	public static HashSet<String> updateStopWords(String... words) {
 		stopWordsSet.addAll(Arrays.asList(words));
 		return stopWordsSet;
+	}
+
+	@FunctionSpec(name = "test_ids", returnType = "set of string")
+	public static HashSet<String> testIds() {
+		return testIds;
 	}
 
 }
